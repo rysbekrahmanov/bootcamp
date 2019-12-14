17957c47d38fa4495c16fd23fd99ed4258611a3a
rysbek <rysbek.rahmanov93@gmail.com>
2019-12-14 22:24:05 +0600

    continue

---
+++ chapter_2/lesson_3/js/main.js
@@ -0,0 +1,47 @@
+// let person = {
+//     name: 'Jack',
+//     sayHello: function(){
+//         let result = 'Hello '+ this.name;
+//         console.log(result);
+
+//         let person2 = {
+//             name: 'Paul ',
+//             sayHello:()=> {
+//                 let result2 = 'Hello +this.name;'
+//             }
+//         }
+//         person2.sayHello();
+//     }
+// }
+
+
+
+// const sayHello = () => {
+//     console.log(this)
+// }
+// sayHello();
+
+// function sayHello(){
+//     console.log(this);
+// }
+
+// sayHello();
+
+// let counter = {
+//     steps : 0,
+//     addStep(){
+//         this.steps++
+//         return this.steps
+//     },
+//     clearSteps(){
+//         this.steps=0;
+//         return this.steps
+
+//     }
+// }
+// counter.addStep();
+// counter.addStep();
+
+// console.log(counter.steps);
+
+  
